a962e65b584fa4cb2c21e86edcf15177fb41be79
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-01-30 00:46:57 +0200

    card style

--- src/NewsCard.jsx
+++ src/NewsCard.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import './NewsCard.sass';
+import './NewsCard.scss';
 
 class NewsCard extends Component {
   render() {
     return (
-      <div className="news-card grid__item grid__item--lg-span-3">news</div>
+      <div className="card-wrapper gitem-lg-3 gitem-md-4 gitem-sm-6">
+      	<div className="news-card">
+			<img src="https://tproger2.azureedge.net/wp-content/uploads/2017/01/linux-console-300x105.jpg"></img>
+      		<p className="cart-title">Не вставляйте в консоль скопированный из Интернета код!</p>
+      		<p className="short-cart-desc">Когда нам нужно быстро найти, как решить какую-то задачу в консоли, мы обычно не задумываясь копируем нужные команды и вставляем их в терминал.</p>
+      	</div>
+      </div>
     );
   }
 }
