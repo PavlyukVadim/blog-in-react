d8cfe2d3c9b137c47adaadcf9a5bb8413d240ff0
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-01-29 12:35:28 +0200

    add sass

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.sass';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+      </header>
+    );
+  }
+}
+
+export default Header;
