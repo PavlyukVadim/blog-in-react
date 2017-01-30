77e4eefc4fa0767e74351d5f1677f60acfa0735e
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-01-30 15:38:09 +0200

    remove static binding

--- src/NewsCard.jsx
+++ src/NewsCard.jsx
@@ -2,29 +2,36 @@ import React, { Component } from 'react';
 import './NewsCard.scss';
 
 class NewsCard extends Component {
-  render() {
-    return (
-      <div className="card-wrapper gitem-lg-3 gitem-md-4 gitem-sm-6">
-      	<div className="news-card">
-			<p className="mob-cart-title">Не вставляйте в консоль скопированный из Интернета код!</p>
-			<div className="img-part">
-				<div className="bg-wrap"></div>
-				<img src="https://tproger2.azureedge.net/wp-content/uploads/2017/01/linux-console-300x105.jpg" alt="news"></img>
-				<p className="stats">
-					<span className="views">
-						<i className="fa fa-eye"></i>
-						<span className="n-views">215</span>
-					</span>	
-					
-					<time dateTime="2011-09-08">08.09.2011</time>
-				</p>
-			</div>
-      		<p className="cart-title">Не вставляйте в консоль скопированный из Интернета код!</p>
-      		<p className="short-cart-desc">Когда нам нужно быстро найти, как решить какую-то задачу в консоли, мы обычно не задумываясь копируем нужные команды и вставляем их в терминал.</p>
-      	</div>
-      </div>
-    );
-  }
+	
+	constructor(props) {
+	    super(props);
+  	}
+
+
+  	render() {
+	    return (
+		    <div className="card-wrapper gitem-lg-3 gitem-md-4 gitem-sm-6">
+		      	<div className="news-card">
+					<p className="mob-cart-title">{ this.props.info.title }</p>
+					<div className="img-part">
+						<div className="bg-wrap"></div>
+						<img src={ this.props.info.image } alt="news"></img>
+						<p className="stats">
+							<span className="views">
+								<i className="fa fa-eye"></i>
+								<span className="n-views">{ this.props.info.views }</span>
+							</span>	
+							
+							<time dateTime={ this.props.info.date }>{ this.props.info.date }</time>
+						</p>
+					</div>
+		      		<p className="cart-title">{ this.props.info.title }</p>
+		      		<p className="short-cart-desc">{ this.props.info.describe }</p>
+		      	</div>
+	      	</div>
+	    );
+	}
+
 }
 
 export default NewsCard;
--- src/NewsContent.jsx
+++ src/NewsContent.jsx
@@ -3,17 +3,71 @@ import React, { Component } from 'react';
 import NewsCard from './NewsCard.jsx';
 import './grid.scss';
 
+
+
+var NEWS = [
+    {
+        id: 1,
+        title: 'Не вставляйте в консоль скопированный из Интернета код!',
+        describe: 'Когда нам нужно быстро найти, как решить какую-то задачу в консоли, мы обычно не задумываясь копируем нужные команды и вставляем их в терминал.',
+        image: 'https://tproger2.azureedge.net/wp-content/uploads/2017/01/linux-console-300x105.jpg',
+        views: 127,
+        date: '21.01.2017'
+    },
+    {
+        id: 2,
+        title: 'Долгожители в сфере IT: самая старая из действующих программ и другие ветераны вычислительного труда',
+        describe: 'Держать ПО в должном состоянии в течение даже нескольких лет без постоянных обновлений практически невозможно. ',
+        image: 'https://tproger2.azureedge.net/wp-content/uploads/2017/01/cobol.png',
+        views: 45,
+        date: '01.01.2010'
+    },
+    {
+        id: 3,
+        title: 'Гаджеты, которые мы ждём в 2017 году',
+        describe: 'Новому году — новые гаджеты! Некоторые технологические новинки уже официально анонсированы, о других пока ходят только слухи, а пользователи с нетерпением ждут и первых, и вторых.',
+        image: 'https://tproger2.azureedge.net/wp-content/uploads/2017/01/gadgets-2017-770.jpg',
+        views: 78,
+        date: '30.02.2017'
+    },
+    {
+        id: 4,
+        title: 'Задача на перегрузку функций в C++, которая может оказаться сложнее, чем выглядит',
+        describe: 'Предположим, у нас есть два класса:... ',
+        image: 'https://tproger2.azureedge.net/wp-content/uploads/2017/01/i2NKb_croper_ru.jpeg',
+        views: 452,
+        date: '12.05.2016'
+    }
+
+];
+
+
+
+
+
 class NewsContent extends Component {
-  render() {
-    return (
-    	<div className="grid">
-	     	<NewsCard></NewsCard>
-	        <NewsCard></NewsCard>
-	        <NewsCard></NewsCard>
-	        <NewsCard></NewsCard>
-        </div>
-    );
-  }
+	constructor(props) {
+	    super(props);
+	    this.news = NEWS;
+  	}
+
+
+
+
+
+	render() {
+		let news = this.news.map((news) => {
+			return <NewsCard info={news} />
+		});
+
+		return (
+			<div className="grid">
+				{ news } 
+		    </div>
+		);
+  	}
+
+
 }
 
 export default NewsContent;
