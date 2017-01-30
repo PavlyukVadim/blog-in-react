a304b7dfe6da7e89e3480bc48b6737175c4ba833
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-01-30 13:17:04 +0200

    media for cards

--- src/NewsCard.jsx
+++ src/NewsCard.jsx
@@ -6,6 +6,7 @@ class NewsCard extends Component {
     return (
       <div className="card-wrapper gitem-lg-3 gitem-md-4 gitem-sm-6">
       	<div className="news-card">
+			<p className="mob-cart-title">Не вставляйте в консоль скопированный из Интернета код!</p>
 			<img src="https://tproger2.azureedge.net/wp-content/uploads/2017/01/linux-console-300x105.jpg"></img>
       		<p className="cart-title">Не вставляйте в консоль скопированный из Интернета код!</p>
       		<p className="short-cart-desc">Когда нам нужно быстро найти, как решить какую-то задачу в консоли, мы обычно не задумываясь копируем нужные команды и вставляем их в терминал.</p>
