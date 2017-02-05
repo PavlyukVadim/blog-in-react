a13a072bce6c0c4673af6f81fe1b7238ddfa98d5
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-05 15:50:13 +0200

    News component

---
+++ src/components/News.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class News extends Component {
+
+    render() {
+        return (
+            <div className="News">
+            	<h1>News</h1>
+          	</div>
+        );
+    }
+}
+
+export default News;
