92198fc2fa388249489cc963f656ed522f2d19a2
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-05 15:49:46 +0200

    Blog component

---
+++ src/components/Blog.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Blog extends Component {
+
+    render() {
+        return (
+            <div className="Blog">
+            	<h1>Blog</h1>
+          	</div>
+        );
+    }
+}
+
+export default Blog;
