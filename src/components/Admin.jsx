e13e94f6da284efa3149cde98a842e1af8a19a88
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-05 15:49:17 +0200

    initial Admin component

---
+++ src/components/Admin.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Admin extends Component {
+
+    render() {
+        return (
+            <div className="Admin">
+            	<h1>Admin</h1>
+          	</div>
+        );
+    }
+}
+
+export default Admin;
