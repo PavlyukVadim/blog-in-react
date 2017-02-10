cb72e7f697269fe44badbfccc48f35442a4983b9
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-10 16:07:38 +0200

    configured server

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const express = require('express'),
 var app = express();
 
 
+app.use(morgan('dev'));
+
 app.use(express.static('./build'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+ 	res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-app.listen(config.get('port'));
+app.listen(config.get('port'), config.get('hostname'), function(){
+	console.log(`Server running at http://${config.get('hostname')}:${config.get('port')}/`);
+});
