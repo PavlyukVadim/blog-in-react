22a878e932c8f405dcca712d31f53f7b1ef10f58
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-12 12:07:06 +0200

    NewsPageContainer

--- src/containers/NewsPageContainer.js
+++ src/containers/NewsPageContainer.js
@@ -5,13 +5,25 @@ import NewsPage from '../components/NewsPage.jsx';
 
 class NewsPageContainer extends Component {
 
+	
+	getPostById = (id) => {
+		var myHeaders = new Headers({
+		    "Content-Type": "json/plain",
+		    "X-Custom-Header": "ProcessThisImmediately",
+		});
 
+
+		return fetch(`http://localhost:9000/posts/${id}`,{ method: 'GET',
+               mode: 'cors',
+               cache: 'default' })		
+      	.then(response => response.json())
+	};
 
 
     render() {
         return (
         	<div>
-    			<NewsPage postId={this.props.params.postId}/>
+    			<NewsPage postId={this.props.params.postId} getPostById={this.getPostById}/>
   			</div>
         );
     }
