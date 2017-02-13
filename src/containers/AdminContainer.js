fb32ba3d469e60ab1b5d761f1b100bb09073aa54
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-13 16:22:20 +0200

    add AdminContainer

---
+++ src/containers/AdminContainer.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import Admin from '../components/Admin.jsx';
+
+
+class AdminContainer extends Component {
+
+	
+  APIAccess = {
+    
+    getPosts : () => {
+      var myHeaders = new Headers({
+          "Content-Type": "json/plain",
+          "X-Custom-Header": "ProcessThisImmediately",
+      });
+
+
+      return fetch(`http://localhost:9000/posts`,{
+                method: 'GET',
+                headers: myHeaders,
+                mode: 'cors',
+                cache: 'default' })   
+              .then(response => response.json())
+    }
+
+
+  }
+
+
+	/*getPostById = (id) => {
+		var myHeaders = new Headers({
+		    "Content-Type": "json/plain",
+		    "X-Custom-Header": "ProcessThisImmediately",
+		});
+
+
+		return fetch(`http://localhost:9000/posts/${id}`,{
+              method: 'GET',
+              headers: myHeaders,
+              mode: 'cors',
+              cache: 'default' })		
+      	   .then(response => response.json())
+	};*/
+
+  
+
+
+
+    render() {
+        return (
+        	 <div>
+    			   <Admin APIAccess={this.APIAccess}/>
+  			   </div>
+        );
+    }
+}
+
+
+export default AdminContainer;
