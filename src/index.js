fba895ced0b6601ff538274643dc4b338a7b98a2
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-18 19:20:15 +0200

    change routes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import App from './components/App.jsx';
 import Blog from './components/Blog.jsx';
 import News from './components/News.jsx';
+import PostEditor from './components/PostEditor.jsx';
+import Admin from './components/Admin.jsx';
 import NewsPageContainer from './containers/NewsPageContainer';
 import AdminContainer from './containers/AdminContainer';
 
@@ -39,7 +41,11 @@ ReactDOM.render(
       			<IndexRoute component={Blog} />
             <Route path='posts/:postId' component={NewsPageContainer} />
 	        	<Route path='news' component={News} />
-            <Route path='admin' component={AdminContainer} />
+            <Route path='admin' component={AdminContainer}>
+              <IndexRoute component={Admin} />
+              <Route path='edit' component={PostEditor} />
+              <Route path='edit/:postId' component={PostEditor} />
+            </Route>
         	</Route> 
     	</Router>
   	</Provider>,
