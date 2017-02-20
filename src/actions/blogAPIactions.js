83bd844ef89489aa8a8bc7fef57f4141c8ea2407
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-20 23:37:05 +0200

    blogAPIactions

---
+++ src/actions/blogAPIactions.js
@@ -0,0 +1,61 @@
+import fetch from 'isomorphic-fetch';
+
+
+export const REQUEST_BLOG_POSTS = 'REQUEST_BLOG_POSTS';
+export const RECEIVE_BLOG_POSTS = 'RECEIVE_BLOG_POSTS';
+
+
+export function requestBlogPosts() {
+	return {
+		type: REQUEST_BLOG_POSTS
+	}
+}
+
+
+export function receiveBlogPosts(json) {
+	return {
+		type: RECEIVE_BLOG_POSTS,
+		posts: json,
+		receivedAt: Date.now()
+	}
+}
+
+
+export function fetchBlogPosts() {
+	return function (dispatch) {
+    	dispatch(requestBlogPosts());
+
+    	let myHeaders = new Headers({
+			"Content-Type": "json/plain",
+			"X-Custom-Header": "ProcessThisImmediately",
+		});
+
+    	return fetch(`http://localhost:9000/posts`,{
+                method: 'GET',
+                headers: myHeaders,
+                mode: 'cors',
+                cache: 'default' })   
+           .then(response => response.json())
+  	       .then(json => dispatch( receiveBlogPosts(json)) )
+  	}
+}
+
+function shouldFetchBlogPosts(state) {
+	const posts = state.blogPosts;
+	if (!posts) {
+		return true;
+	} else if (posts.isFetching) {
+		return false;
+	}
+	return true; 
+}
+
+export function fetchBlogPostsIfNeeded() {
+	return (dispatch, getState) => {
+		if (shouldFetchBlogPosts( getState() )) {
+			return dispatch( fetchBlogPosts() );
+		} else {
+			return Promise.resolve()
+		}
+	}
+}
