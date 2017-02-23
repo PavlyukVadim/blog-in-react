866182d1ae1c3b58bbe584cfc0ef68b6ff1e8143
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-02-23 17:25:14 +0200

    add Preloader component

---
+++ src/components/GeneralPreloader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Preloader } from 'react-materialize';
+import './GeneralPreloader.scss';
+
+export default () => {
+  return (
+    <div className="preloader">
+    	<Preloader flashing/>
+	</div>
+  );
+};
