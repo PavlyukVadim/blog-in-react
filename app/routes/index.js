78642e36f79f20448792406b0e7e6eacd100d5e4
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-07-30 12:30:02 +0300

    code style

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,69 +1,65 @@
-"use strict";
-var express = require('express');
-var bodyParser = require('body-parser');
-var posts = require('../../testDb.js');
+'use strict';
 
-module.exports = function(app) {
+const express = require('express');
+const bodyParser = require('body-parser');
+const posts = require('../../testDb.js');
 
-	var postRouter = express.Router();
-	postRouter.use(bodyParser.json());
+module.exports = (app) => {
 
+  const postRouter = express.Router();
+  postRouter.use(bodyParser.json());
 
-	postRouter.route('/')
-	.all(function(req,res,next) {
-	    res.writeHead(200, { 'Content-Type': 'application/json' })
-	    next();
-	})
+  postRouter.route('/')
+  .all((req,res,next) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    next();
+  })
+  .get((req,res,next) => {
+    res.end(JSON.stringify(posts));
+  })
+  .post((req, res, next) => {
+    const post = {
+      id: posts.length + 1,
+      title: req.body.title,
+      describe: req.body.describe,
+      image: req.body.image,
+      views: 0,
+      date: Date.now(),
+    };
+    posts.push(post);
+    res.end(JSON.stringify(posts));
+  })
+  .delete((req, res, next) => {
+    posts.length = 0;
+    res.end('Posts deleted');
+  });
 
-	.get(function(req,res,next){
-	    res.end(JSON.stringify(posts));
-	})
+  postRouter.route('/:postId')
+  .all((req,res,next) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain',
+      }
+    );
+    next();
+  })
+  .get((req,res,next) => {
+    res.end(JSON.stringify(posts[req.params.postId - 1]));
+  })
+  .put((req, res, next) => {
+    posts[req.params.postId - 1] = {
+      id: req.params.postId,
+      title: req.body.title,
+      describe: req.body.describe,
+      image: req.body.image,
+      views: req.body.views,
+      date: req.body.date,
+    }; 
+    res.end(JSON.stringify(posts));
+  })
+  .delete((req, res, next) => {
+    delete posts[req.params.postId - 1];
+    res.end(JSON.stringify(posts));
+  });
 
-	.post(function(req, res, next){
-	    let post = {
-	    	id: posts.length + 1,
-	        title: req.body.title,
-	        describe: req.body.describe,
-	        image: req.body.image,
-	        views: 0,
-	        date: Date.now()
-	    }
-	    posts.push(post);
-	  	res.end(JSON.stringify(posts));
-	})
-
-	.delete(function(req, res, next){
-		posts.length = 0;
-	    res.end('Posts deleted');
-	});
-
-
-	postRouter.route('/:postId')
-	.all(function(req,res,next) {
-		res.writeHead(200, { 'Content-Type': 'text/plain' });
-		next();
-	})
-
-	.get(function(req,res,next){
-	    res.end( JSON.stringify(posts[req.params.postId - 1]) );
-	})
-
-	.put(function(req, res, next){
-	  	posts[req.params.postId - 1] = {
-	    	id: req.params.postId,
-	        title: req.body.title,
-	        describe: req.body.describe,
-	        image: req.body.image,
-	        views: req.body.views,
-	        date: req.body.date
-	  	}  
-	    res.end(JSON.stringify(posts));
-	})
-
-	.delete(function(req, res, next){
-		delete posts[req.params.postId - 1];
-	    res.end(JSON.stringify(posts));
-	});
-
-	app.use('/posts', postRouter);
+  app.use('/posts', postRouter);
 }
