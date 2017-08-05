32f2b09607d1980e02a408192472fd78e66bec8f
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-08-05 16:56:07 +0300

    change footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
-import { Footer } from 'react-materialize';
+import styles from './Footer.scss';
 
 class MyFooter extends Component {
   render() {
     return (
-      <Footer
-        className='example'
-        copyrights="&copy; 2017 Copyright Text"
-        moreLinks={<a className="grey-text text-lighten-4 right" href="#!">More Links</a>}
-        links={
-          <ul>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-          </ul>}
-      >
-        <h5 className="white-text">Footer Content</h5>
-        <p className="grey-text text-lighten-4">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, quaerat!
-        </p>
-      </Footer>
+      <footer>
+        <p>&copy; 2017 Copyright Text</p>
+      </footer>
     );
   }
 }
