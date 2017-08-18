259b264cc7f7f477558192fafb57197f962f15bb
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-08-18 23:34:10 +0300

    add .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "extends": [
+    "react-app",
+    "airbnb"
+  ],
+  "rules": {
+    "no-tabs": 2,
+    "react/prop-types": 0,
+    "react/jsx-filename-extension": 1,
+    "no-unused-vars": 1,
+    "arrow-body-style": [
+      "error",
+      "as-needed"
+    ],
+    "react/prefer-stateless-function": 0
+  }
+};
